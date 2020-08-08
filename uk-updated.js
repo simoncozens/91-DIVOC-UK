807614d7eefd8810b39da84aa9b52afca8ed61f6
Simon Cozens <simon@simon-cozens.org>
2020-08-08 20:14:51 +0100

    2020-08-08

--- uk-updated.js
+++ uk-updated.js
@@ -1,4 +1,4 @@
-_dateUpdated = "08/01/2020";
-_reqStr = "20200801";
+_dateUpdated = "08/08/2020";
+_reqStr = "20200808";
 
-$("#uk-updated").html("(Updated: 08/01 at 03:22pm)");
+$("#uk-updated").html("(Updated: 08/08 at 08:14pm)");
